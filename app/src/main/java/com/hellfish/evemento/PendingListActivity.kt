df539e96846ccb87b15f40d94af796edaaf7f111
Nicolas <nicolas@Nick.NickDom>
2018-05-14 20:02:30 -0300

    Teniendo una lista de pendientes...

---
+++ app/src/main/java/com/hellfish/evemento/PendingListActivity.kt
@@ -0,0 +1,12 @@
+package com.hellfish.evemento
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PendingListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pending_list)
+    }
+}
