01977336df50372d1bcb81a4b341372fa3780f1e
Juan Manuel Fernandes dos Santos <juan.fernandes@10pines.com>
2018-05-20 23:14:33 -0300

    Use RecyclerAdapter to simplify PollAdapter and AnswersAdapter

--- app/src/main/java/com/hellfish/evemento/PollsActivity.kt
+++ app/src/main/java/com/hellfish/evemento/PollsActivity.kt
@@ -1,17 +1,14 @@
 package com.hellfish.evemento
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_polls.*
-import kotlinx.android.synthetic.main.answer_polls.view.*
 import kotlinx.android.synthetic.main.content_polls.view.*
 
 class PollsActivity : AppCompatActivity() {
@@ -20,62 +17,40 @@ class PollsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_polls)
 
-        val viewManager = LinearLayoutManager(this)
         val polls = listOf(Poll("¿Comes carne?", listOf("Sí", "No")))
-        val viewAdapter = PollAdapter(polls)
+
         recyclerView.apply {
-            layoutManager = viewManager
-            adapter = viewAdapter
+            layoutManager = LinearLayoutManager(context)
+            adapter = PollAdapter(polls)
         }
     }
 
     data class Poll(val question: String, val answers: List<String>)
 
-    class PollAdapter(val polls: List<Poll>) : RecyclerView.Adapter<PollAdapter.ViewHolder>() {
-        class ViewHolder(val cardView: CardView) : RecyclerView.ViewHolder(cardView)
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val poll = polls[position]
-            holder.cardView.question.text = poll.question
-            holder.cardView.answers.adapter = AnswersAdapter(poll.answers)
+    class PollAdapter(polls: List<Poll>) : RecyclerAdapter<CardView, Poll>(polls) {
+        override fun layout(): Int {
+            return R.layout.content_polls
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val cardView = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.content_polls, parent, false) as CardView
-
-            cardView.answers.apply {
-                layoutManager = LinearLayoutManager(parent.context)
+        override fun doOnItemOnBindViewHolder(view: CardView, item: Poll, context: Context) {
+            view.question.text = item.question
+            view.answers.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = AnswersAdapter(item.answers)
             }
-
-            return ViewHolder(cardView)
-        }
-
-        override fun getItemCount(): Int {
-            return polls.size
         }
     }
 
-    class AnswersAdapter(val answers: List<String>) : RecyclerView.Adapter<AnswersAdapter.ViewHolder>() {
-        class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class AnswersAdapter(answers: List<String>) : RecyclerAdapter<TextView, String>(answers) {
+        override fun layout() : Int {
+            return R.layout.answer_polls
+        }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val textView = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.answer_polls, parent, false) as TextView
-
-            textView.setOnClickListener { v ->
-                Toast.makeText(parent.context, "Elegiste ${v.textView.text}!", Toast.LENGTH_SHORT).show()
+        override fun doOnItemOnBindViewHolder(view: TextView, item: String, context: Context) {
+            view.text = item
+            view.setOnClickListener {
+                Toast.makeText(context, "Elegiste ${view.text}!", Toast.LENGTH_SHORT).show()
             }
-
-            return ViewHolder(textView)
-        }
-
-        override fun getItemCount(): Int {
-            return answers.size
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.textView.text = answers[position]
         }
     }
 }
--- app/src/main/java/com/hellfish/evemento/RecyclerAdapter.kt
+++ app/src/main/java/com/hellfish/evemento/RecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.hellfish.evemento
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +9,15 @@ import android.view.ViewGroup
 abstract class RecyclerAdapter<V: View,
                                     Item>(open val items: List<Item>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder<V>>() {
 
+    private var context : Context? = null
+
     class ViewHolder<out V : View>(val view: V) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<V> {
         val view = LayoutInflater.from(parent.context)
                 .inflate(layout(), parent, false) as V
+
+        this.context = parent.context
 
         return ViewHolder(view)
     }
@@ -22,10 +27,12 @@ abstract class RecyclerAdapter<V: View,
     }
 
     override fun onBindViewHolder(holder: ViewHolder<V>, position: Int) : Unit {
-        doOnItemOnBindViewHolder(holder.view, items[position])
+        this.context?.let {
+            context -> doOnItemOnBindViewHolder(holder.view, items[position], context)
+        }
     }
 
     abstract fun layout() : Int
 
-    abstract fun doOnItemOnBindViewHolder(view: V, item: Item) : Unit
+    abstract fun doOnItemOnBindViewHolder(view: V, item: Item, context: Context) : Unit
 }
