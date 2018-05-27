138d988b55c3b9fcf90144d200b690619b29ef08
GastonTS <gaston.santoalla@despegar.com>
2018-05-27 20:33:04 -0300

    Change Polls -> Poll (should be singular)

--- app/src/main/java/com/hellfish/evemento/PollActivity.kt
+++ app/src/main/java/com/hellfish/evemento/PollActivity.kt
@@ -1,28 +1,21 @@
 package com.hellfish.evemento
 
 import android.content.Context
-import android.graphics.Rect
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 
-import kotlinx.android.synthetic.main.activity_polls.*
-import kotlinx.android.synthetic.main.closed_answer_polls.view.*
-import kotlinx.android.synthetic.main.content_polls.view.*
-import kotlinx.android.synthetic.main.open_answer_polls.view.*
+import kotlinx.android.synthetic.main.activity_poll.*
+import kotlinx.android.synthetic.main.poll_content.view.*
 
-class PollsActivity : AppCompatActivity() {
+class PollActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_polls)
+        setContentView(R.layout.activity_poll)
 
         val polls = mutableListOf(Poll.Open("¿Comes carne?", listOf(Answer.Open("Sí", 2), Answer.Open("No", 1))),
                                     Poll.Closed("¿Llevas mascota?",
@@ -66,7 +59,7 @@ class PollsActivity : AppCompatActivity() {
         }
 
         override fun layout(viewType : Int): Int {
-            return R.layout.content_polls
+            return R.layout.poll_content
         }
 
         override fun getItemView(item: Poll): Int {
@@ -95,7 +88,7 @@ class PollsActivity : AppCompatActivity() {
 
     class OpenAnswersAdapter(private val callback: (Answer.Open) -> Unit, answers: List<Answer.Open>) : RecyclerAdapter<TextView, Answer.Open>(answers) {
         override fun layout(viewType : Int) : Int {
-            return R.layout.open_answer_polls
+            return R.layout.poll_open_answer
         }
 
         override fun doOnItemOnBindViewHolder(view: TextView, item: Answer.Open, context: Context) {
@@ -109,7 +102,7 @@ class PollsActivity : AppCompatActivity() {
 
     class ClosedAnswersAdapter(answers: List<Answer.Closed>, val totalAmount: Int) : RecyclerAdapter<TextView, Answer.Closed>(answers) {
         override fun layout(viewType : Int) : Int {
-            return R.layout.closed_answer_polls
+            return R.layout.poll_closed_answer
         }
 
         override fun doOnItemOnBindViewHolder(view: TextView, item: Answer.Closed, context: Context) {
