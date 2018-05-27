39aa3d88f0806636c9b66c3ad72d24c371a014e0
GastonTS <gaston.santoalla@despegar.com>
2018-05-27 20:39:27 -0300

    Change Pending and Tasks for Task

--- app/src/main/java/com/hellfish/evemento/TaskListActivity.kt
+++ app/src/main/java/com/hellfish/evemento/TaskListActivity.kt
@@ -3,10 +3,10 @@ package com.hellfish.evemento
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class PendingListActivity : AppCompatActivity() {
+class TaskListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pending_list)
+        setContentView(R.layout.activity_task_list)
     }
 }
