7a6bd92df4e143c601b19a4e64e529b65d9777a8
Nicolas Schteinschraber <nicoschtein@gmail.com>
2018-05-29 15:46:28 -0300

    Implementacion de EventList: EventListFragment y EventListAdapter con el layout de las cards. De paso agregue un extension para practicar.

---
+++ app/src/main/java/com/hellfish/evemento/event/list/EventListAdapter.kt
@@ -0,0 +1,43 @@
+package com.hellfish.evemento.event
+import com.hellfish.evemento.R
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+
+import com.hellfish.evemento.extensions.inflate
+
+import kotlinx.android.synthetic.main.fragment_event_list_item.view.*
+
+class EventListAdapter(private val events: ArrayList<Event>) : RecyclerView.Adapter<EventListAdapter.EventHolder>() {
+
+    override fun getItemCount(): Int {
+        return events.size
+    }
+
+    override fun onBindViewHolder(holder: EventListAdapter.EventHolder, position: Int) {
+        val event = events[position]
+        holder.bindEvent(event)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventListAdapter.EventHolder{
+        val inflatedView = parent.inflate(R.layout.fragment_event_list_item)
+        return EventHolder(inflatedView)
+    }
+
+    class EventHolder(v:View) : RecyclerView.ViewHolder(v) {
+
+        private var view : View = v
+        private var event : Event? = null
+
+        fun bindEvent(event: Event) {
+            this.event = event
+            view.text_title.text = event.title
+            view.text_desc.text = event.description
+            view.text_date.text = event.time
+            // TODO: Bindear todos los valores del evento que faltan cuando este completo el layout.
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/hellfish/evemento/event/list/EventListFragment.kt
@@ -0,0 +1,90 @@
+package com.hellfish.evemento.event.list
+
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hellfish.evemento.NavigatorFragment
+import com.hellfish.evemento.R
+import com.hellfish.evemento.event.Event
+import com.hellfish.evemento.event.EventListAdapter
+
+
+
+class EventListFragment : NavigatorFragment() {
+
+    lateinit var eventsRecyclerView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_event_list, container, false)
+
+        eventsRecyclerView = view.findViewById(R.id.events_recycler_view)
+
+        val manager = LinearLayoutManager(context)
+        eventsRecyclerView.setLayoutManager(manager)
+
+        // TODO: Usar los eventos pasados como argumento del fragment, por ahora usa eventos hardcoded
+        eventsRecyclerView.adapter = EventListAdapter(arrayListOf(
+                Event("Mock Title 1",
+                        "Mock Description 1",
+                        "Mock Time 1",
+                        "Mock Location",
+                        listOf("Juan", "Juan", "Juan"),
+                        listOf("rides"),
+                        listOf("tasks"),
+                        listOf("polls"),
+                        listOf("comments")),
+                Event("Mock Title 2",
+                        "Mock Description 2",
+                        "Mock Time 2",
+                        "Mock Location",
+                        listOf("Juan", "Juan", "Juan"),
+                        listOf("rides"),
+                        listOf("tasks"),
+                        listOf("polls"),
+                        listOf("comments")),
+                Event("Mock Title 3",
+                        "Mock Description 3",
+                        "Mock Time 3",
+                        "Mock Location",
+                        listOf("Juan", "Juan", "Juan"),
+                        listOf("rides"),
+                        listOf("tasks"),
+                        listOf("polls"),
+                        listOf("comments")),
+                Event("Mock Title 4",
+                        "Mock Description 4",
+                        "Mock Time 4",
+                        "Mock Location",
+                        listOf("Juan", "Juan", "Juan"),
+                        listOf("rides"),
+                        listOf("tasks"),
+                        listOf("polls"),
+                        listOf("comments")),
+                Event("Mock Title 5",
+                        "Mock Description 5",
+                        "Mock Time 5",
+                        "Mock Location",
+                        listOf("Juan", "Juan", "Juan"),
+                        listOf("rides"),
+                        listOf("tasks"),
+                        listOf("polls"),
+                        listOf("comments")),
+                Event("Mock Title 6",
+                        "Mock Description 6",
+                        "Mock Time 6",
+                        "Mock Location",
+                        listOf("Juan", "Juan", "Juan"),
+                        listOf("rides"),
+                        listOf("tasks"),
+                        listOf("polls"),
+                        listOf("comments"))))
+
+        return view
+
+    }
+}
