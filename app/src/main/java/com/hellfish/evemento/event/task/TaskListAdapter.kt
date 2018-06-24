db2683edf765d371efef5df4aadfac76fb5dff64
Nico B <nicolasmbrana@gmail.com>
2018-06-24 17:42:59 -0300

    Showing the Task Items

--- app/src/main/java/com/hellfish/evemento/event/task/TaskListAdapter.kt
+++ app/src/main/java/com/hellfish/evemento/event/task/TaskListAdapter.kt
@@ -1,18 +1,18 @@
 package com.hellfish.evemento.event.task
 
 import android.content.Context
-import android.support.design.widget.CoordinatorLayout
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import com.hellfish.evemento.R
 import com.hellfish.evemento.RecyclerAdapter
 import kotlinx.android.synthetic.main.fragment_task_list_item.view.*
 
-class TaskListAdapter(private val taskItems: MutableList<TaskItem>) : RecyclerAdapter<CoordinatorLayout, TaskItem?>(taskItems) {
+class TaskListAdapter(private val taskItems: MutableList<TaskItem>) : RecyclerAdapter<CardView, TaskItem?>(taskItems) {
     override fun layout(item: Int): Int {
-        return R.layout.fragment_task_list
+        return R.layout.fragment_task_list_item
     }
 
-    override fun doOnItemOnBindViewHolder(view: CoordinatorLayout, item: TaskItem?, context: Context) {
+    override fun doOnItemOnBindViewHolder(view: CardView, item: TaskItem?, context: Context) {
         view.task_item?.text = item?.description
         view.task_item_owner?.text = item?.owner
     }
