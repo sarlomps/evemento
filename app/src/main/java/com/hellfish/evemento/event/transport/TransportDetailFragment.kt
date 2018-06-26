de92ec500ab8e0eaa37420fa93404aaaae44b44c
JuanchiRios <juan.rios@despegar.com>
2018-06-25 22:29:55 -0300

    adding remove to transportDetail

--- app/src/main/java/com/hellfish/evemento/event/transport/TransportDetailFragment.kt
+++ app/src/main/java/com/hellfish/evemento/event/transport/TransportDetailFragment.kt
@@ -12,10 +12,11 @@ import android.widget.TextView
 import com.hellfish.evemento.EventViewModel
 import com.hellfish.evemento.NavigatorFragment
 import com.hellfish.evemento.R
+import com.hellfish.evemento.extensions.withDrawable
 import kotlinx.android.synthetic.main.fragment_transport_detail.*
 import kotlinx.android.synthetic.main.fragment_transport_detail.view.*
 
-class TransportDetailFragment() : NavigatorFragment(){
+class TransportDetailFragment() : NavigatorFragment() {
 
     lateinit var driver: UserMiniDetail
     private lateinit var loggedInUser: UserMiniDetail
@@ -26,17 +27,17 @@ class TransportDetailFragment() : NavigatorFragment(){
         super.onCreate(savedInstanceState)
         eventViewModel = ViewModelProviders.of(activity!!).get(EventViewModel::class.java)
         eventViewModel.rides.observe(this, Observer { rides ->
-            transport = rides?.find{driver.sameUser(it.driver)}!!
+            transport = rides?.find { driver.sameUser(it.driver) }!!
             llTransportDetail.removeAllViews()
             transport.passangers.forEach { addPassanger(view!!, it) }
-            toogleFabIfTransportIsFull(transport)
+            toogleFabIfNecessary(transport)
 
         })
     }
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_transport_detail, container, false)
+            inflater.inflate(R.layout.fragment_transport_detail, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -47,12 +48,7 @@ class TransportDetailFragment() : NavigatorFragment(){
         if (argDriver != null) {
             driver = argDriver
             view.txtDriverName.text = driver.nickname
-
-            transport_detail_fab.setOnClickListener{
-                transport.passangers.add(loggedInUser)
-                eventViewModel.edit(transport)
-            }
-
+            if (::transport.isInitialized) toogleFabIfNecessary(transport)
         }
     }
 
@@ -65,7 +61,7 @@ class TransportDetailFragment() : NavigatorFragment(){
     private fun createPassangerView(context: Context, passanger: UserMiniDetail): TextView {
         return TextView(context).apply {
             layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                                    ViewGroup.LayoutParams.WRAP_CONTENT)
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 setTextAppearance(R.style.Base_TextAppearance_AppCompat_Medium)
             } else {
@@ -75,11 +71,21 @@ class TransportDetailFragment() : NavigatorFragment(){
         }
     }
 
-    private fun toogleFabIfTransportIsFull(transport: TransportItem) {
-        if (transport.isFull() || transport.isAlreadyInTransport(loggedInUser)) {
+    private fun toogleFabIfNecessary(transport: TransportItem) {
+        if (transport.isFull() && !transport.isAlreadyInTransport(loggedInUser))
             transport_detail_fab.hide()
-        } else {
+        else {
             transport_detail_fab.show()
+            if (!transport.isAlreadyInTransport(loggedInUser))
+                transport_detail_fab.withDrawable(R.drawable.ic_person_add_white_24dp).setOnClickListener {
+                    transport.passangers.add(loggedInUser)
+                    eventViewModel.edit(transport)
+                }
+            else
+                transport_detail_fab.withDrawable(R.drawable.ic_remove_white_24dp).setOnClickListener {
+                    transport.passangers.remove(loggedInUser)
+                    eventViewModel.edit(transport)
+                }
         }
     }
 }
