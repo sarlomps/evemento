7c748916fc212b7c782d4377180f4561a5f7de6d
GastonTS <gaston.santoalla@despegar.com>
2018-06-27 22:36:37 -0300

    Comments ABM - no Firebase

--- app/src/main/java/com/hellfish/evemento/event/comment/CommentAdapter.kt
+++ app/src/main/java/com/hellfish/evemento/event/comment/CommentAdapter.kt
@@ -5,12 +5,13 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.widget.*
+import android.view.View
 import com.hellfish.evemento.R
 import com.hellfish.evemento.RecyclerAdapter
 import com.hellfish.evemento.api.Comment
 import kotlinx.android.synthetic.main.comment_content.view.*
 
-class CommentAdapter(comments: MutableList<Comment>) : RecyclerAdapter<CardView, Comment>(comments) {
+class CommentAdapter(comments: MutableList<Comment>, val editListener: (Comment) -> View.OnClickListener) : RecyclerAdapter<CardView, Comment>(comments) {
 
     override fun layout(item : Int): Int {
         return R.layout.comment_content
@@ -23,6 +24,8 @@ class CommentAdapter(comments: MutableList<Comment>) : RecyclerAdapter<CardView,
         view.commentUserName.text = item.name
         view.commentUserName.paintFlags = view.commentUserName.paintFlags or Paint.UNDERLINE_TEXT_FLAG
         view.commentMessage.text = item.message
+
+        view.setOnClickListener(editListener(item))
     }
 
     private fun userColor(userId: String, name: String): Int {
--- app/src/main/java/com/hellfish/evemento/event/comment/CommentFragment.kt
+++ app/src/main/java/com/hellfish/evemento/event/comment/CommentFragment.kt
@@ -1,44 +1,82 @@
 package com.hellfish.evemento.event.comment
 
+import android.app.Dialog
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.hellfish.evemento.EventViewModel
 import com.hellfish.evemento.NavigatorFragment
 import com.hellfish.evemento.R
 import com.hellfish.evemento.R.string.title_fragment_comments_list
-import com.hellfish.evemento.R.string.title_fragment_poll_list
+import com.hellfish.evemento.SessionManager
 import com.hellfish.evemento.api.Comment
 import kotlinx.android.synthetic.main.fragment_comment_list.*
-
-import kotlinx.android.synthetic.main.fragment_poll_list.*
 
 class CommentFragment : NavigatorFragment() {
 
     override val titleId = title_fragment_comments_list
 
     lateinit var eventViewModel: EventViewModel
-
+    lateinit var dialogInput: EditText
+    lateinit var clickDialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         eventViewModel = ViewModelProviders.of(activity!!).get(EventViewModel::class.java)
         eventViewModel.loadComments { _ -> showToast(R.string.errorLoadingComments) }
         eventViewModel.comments.observe(this, Observer { comments ->
-                comments?.let { commentsRecyclerView.adapter = CommentAdapter(it.toMutableList()) }
+            comments?.let { commentsRecyclerView.adapter = CommentAdapter(it.toMutableList(), editListener()) }
         })
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_comment_list, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dialogInput = EditText(activity)
+        clickDialog = createAlertDialog(R.string.commentDialogTitle,
+                dialogInput,
+                negativeButtonDefinition = Pair(R.string.cancel, { _, _ -> Unit }),
+                neutralButtonDefinition = Pair(R.string.delete, { _, _ -> Unit }))
+        return inflater.inflate(R.layout.fragment_comment_list, container, false)
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        commentListFab.setOnClickListener(addListener())
         commentsRecyclerView.layoutManager = LinearLayoutManager(context)
     }
 
+    private fun addListener() = View.OnClickListener {
+        dialogInput.setText("")
+        clickDialog.run {
+            setButton(Dialog.BUTTON_POSITIVE, getString(R.string.accept)) { _, _ ->
+                eventViewModel.add(Comment(
+                        "",
+                        SessionManager.getCurrentUser()!!.userId,
+                        SessionManager.getCurrentUser()!!.displayName,
+                        dialogInput.text.toString()
+                ))
+            }
+            show()
+            getButton(Dialog.BUTTON_NEUTRAL)?.visibility = View.GONE
+        }
+    }
+
+    private fun editListener() = { comment: Comment ->
+        View.OnClickListener {
+            dialogInput.setText(comment.message)
+            clickDialog.run {
+                setButton(Dialog.BUTTON_POSITIVE, getString(R.string.accept)) { _, _ -> eventViewModel.edit(comment.copy(message = dialogInput.text.toString())) }
+                setButton(Dialog.BUTTON_NEUTRAL, getString(R.string.delete)) { _, _ -> eventViewModel.remove(comment) }
+                show()
+                getButton(Dialog.BUTTON_NEUTRAL).visibility = View.VISIBLE
+            }
+        }
+    }
+
 }
+
