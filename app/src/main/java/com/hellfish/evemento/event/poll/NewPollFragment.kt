311423c620adf3ea806d3cf8aabe3744bd9feeaf
juanFdS <juan9794@gmail.com>
2018-07-01 22:39:32 -0300

    Add button to remove answers when creating polls

--- app/src/main/java/com/hellfish/evemento/event/poll/NewPollFragment.kt
+++ app/src/main/java/com/hellfish/evemento/event/poll/NewPollFragment.kt
@@ -2,10 +2,11 @@ package com.hellfish.evemento.event.poll
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.*
 import com.hellfish.evemento.EventViewModel
 import com.hellfish.evemento.NavigatorFragment
 import com.hellfish.evemento.NetworkManager
@@ -14,7 +15,6 @@ import com.hellfish.evemento.R.string.*
 import com.hellfish.evemento.extensions.getChildren
 
 import kotlinx.android.synthetic.main.fragment_new_poll.*
-import kotlinx.android.synthetic.main.poll_content.*
 
 class NewPollFragment : NavigatorFragment() {
 
@@ -44,24 +44,38 @@ class NewPollFragment : NavigatorFragment() {
                 activity!!.onBackPressed()
             })
         }
-        addAnswer()
-        addAnswer()
-        newPollAddAnswerButton.setOnClickListener { _ -> addAnswer() }
+        addAnswer(withDeleteButton=false)
+        addAnswer(withDeleteButton=false)
+        newPollAddAnswerButton.setOnClickListener { _ -> addAnswer(withDeleteButton=true) }
     }
 
-    private fun answers() = newPollAnswersLinearLayout.getChildren<EditText>().map { editText ->
-        Answer.Open(editText.text.toString(), listOf())
-    }
+    private fun answers() =
+            newPollAnswersLinearLayout.getChildren<LinearLayout>().map { it.getChildren<View>().find { it is EditText } as EditText }
+                    .map { editText -> Answer.Open(editText.text.toString(), listOf()) }
 
-    private fun addAnswer() {
-        val newEditTextView = EditText(context)
-        newEditTextView.setSingleLine(true)
-        newEditTextView.hint = "An answer"
-        newPollAnswersLinearLayout.addView(newEditTextView)
+    private fun addAnswer(withDeleteButton : Boolean) {
+        val newEditAnswer = LinearLayout(context)
+        val newEditTextView = EditText(context).apply {
+            layoutParams = TableRow.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.85f)
+            setSingleLine(true)
+            hint = "An answer"
+        }
+        val deleteAnswerButton = Button(context).apply {
+            layoutParams = TableRow.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.1f)
+            background = ContextCompat.getDrawable(context!!, R.drawable.ic_close_grey_24dp)
+            setOnClickListener { newPollAnswersLinearLayout.removeView(newEditAnswer) }
+
+        }
+        newEditAnswer.apply {
+            addView(newEditTextView)
+            if(withDeleteButton) { addView(deleteAnswerButton) }
+        }
+
+        newPollAnswersLinearLayout.addView(newEditAnswer)
     }
 
     private fun validatePollFields() : Boolean {
-        val answers = newPollAnswersLinearLayout.getChildren<EditText>()
+        val answers : List<EditText> = newPollAnswersLinearLayout.getChildren<LinearLayout>().map { it.getChildren<View>().find { it is EditText }!! as EditText }
         val pollFields = answers.plus(editPollQuestion)
         answers.forEach { answer ->
             answer.validateText(errorDuplicatePollAnswer, { answerText ->
